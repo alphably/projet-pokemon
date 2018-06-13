81a996c2c19128dc0c5fb589873336ba6ad2f533
dev <root@email.fr>
2018-06-13 12:26:15 +0200

    mise en cache de la page pokemon dans un fichier

--- init.py
+++ init.py
@@ -4,11 +4,46 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://pokemondb.net/pokedex/all"
-response = requests.get(url)
-html = str(response.content)
+import mysql.connector, random
 
-soup = BeautifulSoup(html, "html.parser")
+conn = mysql.connector.connect(host="localhost",user="root",password="", database="mydb")
+cursor = conn.cursor()
+
+def createTable():
+    cursor.execute("""
+      CREATE TABLE IF NOT EXISTS pokenom
+      (
+         id INT PRIMARY KEY AUTO_INCREMENT,
+         pki VARCHAR(10),
+         name VARCHAR(100),
+         size ENUM('Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
+          'Psychic', 'Bug', 'Rock','Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'),
+         total INT(5),
+         hp INT(5),
+         attack INT(5),
+         defense INT(5),
+         sp_atk INT(5),
+         sp_def(5),
+         speed(5),
+      );
+      """)
+
+
+
+
+
+#url = "https://pokemondb.net/pokedex/all"
+#response = requests.get(url)
+#html = str(response.content)
+
+# Mise en cache de la page HTML
+#fichier = open("pokemon.html", "a")
+#fichier.write(html)
+#fichier.close()
+
+#soup = BeautifulSoup(html, "html.parser")
+with open("pokemon.html") as fp:
+    soup = BeautifulSoup(fp)
 
 tab = soup.find(id="pokedex")
 
