7d1a015b7baa00958835f5070487b81487813611
dev <root@email.fr>
2018-06-13 13:01:54 +0200

    ajout des scripts de creation de la base de donnees

--- init.py
+++ init.py
@@ -6,31 +6,31 @@ from bs4 import BeautifulSoup
 
 import mysql.connector, random
 
-conn = mysql.connector.connect(host="localhost",user="root",password="", database="mydb")
+conn = mysql.connector.connect(host="localhost",user="root", password="", database="base_pokemon")
 cursor = conn.cursor()
 
-def createTable():
+def scriptSQL():
+    cursor.execute("""CREATE DATABASE IF NOT EXISTS base_pokemon; """)
     cursor.execute("""
       CREATE TABLE IF NOT EXISTS pokenom
       (
          id INT PRIMARY KEY AUTO_INCREMENT,
          pki VARCHAR(10),
          name VARCHAR(100),
-         size ENUM('Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
+         type ENUM('Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
           'Psychic', 'Bug', 'Rock','Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'),
          total INT(5),
          hp INT(5),
          attack INT(5),
          defense INT(5),
          sp_atk INT(5),
-         sp_def(5),
-         speed(5),
+         sp_def INT(5),
+         speed INT(5)
       );
       """)
 
 
-
-
+scriptSQL()
 
 #url = "https://pokemondb.net/pokedex/all"
 #response = requests.get(url)
@@ -42,6 +42,8 @@ def createTable():
 #fichier.close()
 
 #soup = BeautifulSoup(html, "html.parser")
+
+# recuperation du fichier pokemon
 with open("pokemon.html") as fp:
     soup = BeautifulSoup(fp)
 
