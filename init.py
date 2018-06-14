2215ff3b2ac92576bfb7036789dd2684cfd37c44
dev <root@email.fr>
2018-06-14 10:35:22 +0200

    correction de l insertion en base de donnees des pokemon

--- init.py
+++ init.py
@@ -12,12 +12,12 @@ cursor = conn.cursor()
 def scriptSQL():
     cursor.execute("""CREATE DATABASE IF NOT EXISTS base_pokemon; """)
     cursor.execute("""
-      CREATE TABLE IF NOT EXISTS pokenom
+      CREATE TABLE IF NOT EXISTS pokemon
       (
          id INT PRIMARY KEY AUTO_INCREMENT,
          pki VARCHAR(10),
          name VARCHAR(100),
-         type ENUM('Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
+         type_pok ENUM('Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
           'Psychic', 'Bug', 'Rock','Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'),
          total INT(5),
          hp INT(5),
@@ -43,30 +43,37 @@ scriptSQL()
 
 #soup = BeautifulSoup(html, "html.parser")
 
-# recuperation du fichier pokemon
-with open("pokemon.html") as fp:
-    soup = BeautifulSoup(fp)
 
 # recuperation du fichier pokemon
+#soup = None
+#
+# fichier = open("pokemon.html", "r")
+# soup = BeautifulSoup(fichier.read())
+# fichier.close()
+
 with open("pokemon.html") as fp:
-    soup = BeautifulSoup(fp)
+    html_doc = fp.read()
+#print(html_doc)
+
+soup = BeautifulSoup(html_doc, "html.parser")
+
+#print(soup)
+#soup = BeautifulSoup("<html>data</html>")
 
 tab = soup.find(id="pokedex")
+#print(dir(tab))
 
 for link in tab.find_all("tr"):
     tt = []
     for l in link.find_all("td"):
         tt.append(l.text)
 
-    #
+    #print(tt)
+    if len(tt) > 0:
+        #print(tt)
+        #exit()
+        cursor.execute("INSERT INTO pokemon (pki, name, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed) "
+                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tt)
 
-    print(type(tt))
-    cursor.execute("INSERT INTO pokemon (pki, name, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed) "
-                   "VALUES (%(pki)s, %(name)s, %(type_pok)s, %(total)s, %(hp)s, %(attack)s, %(defense)s, %(sp_atk)s, %(sp_def)s, %(speed)s)", tt)
-
-
-#     data = []
-    #
-
-    #print("\n Message de fin")
-    #print("\n")
+conn.commit()
+conn.close()
